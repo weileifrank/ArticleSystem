6d573700e8a3ab134923f1f3d39f1bd6e56193ee
weileifrank <w1138289316@qq.com>
2018-10-31 17:25:58 +0800

    10.31

--- controllers/articleController.go
+++ controllers/articleController.go
@@ -54,6 +54,12 @@ func (this *ArticleController) ShowList() {
 	//获取文章类型
 	var types []models.ArticleType
 	conn, e := redis.Dial("tcp", ":6379")
+	strings, i := redis.Strings(conn.Do("mget", "key1", "key2"))
+	if i != nil {
+		beego.Info("redis.Strings err", i)
+		return
+	}
+	beego.Info("strings:=======================>", strings)
 	if e != nil {
 		beego.Error("redis连接错误")
 		return
